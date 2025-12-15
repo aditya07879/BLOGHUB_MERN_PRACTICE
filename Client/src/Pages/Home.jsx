ef454cb76e72214dcb2acadfdf08f7da0a0f5332
aditya07879 <aditya07879@gmail.com>
2025-12-15 22:26:10 +0530

    Add header component

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Header from '../Components/Header'
 
 function Home() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
