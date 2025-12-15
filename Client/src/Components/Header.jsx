c0ac3c8c1a4738f4863aa056fbd0baabe03c9558
aditya07879 <aditya07879@gmail.com>
2025-12-15 23:01:29 +0530

    Design Complete header Componet!

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -5,10 +5,18 @@ function Header() {
   return (
     <div className='mx-8 sm:mx-16 xl:mx-24 relative'> 
         <div className='text-center mt-20 mb-8'>
-            <div className='inline-flex item-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40  bg-primary/10 rounded-full text-sm text-primary'>
+            <div className='inline-flex item-center justify-center gap-4 px-6 py-1.5 mb-7 border border-primary/40  bg-primary/10 rounded-full text-sm text-primary -translate-x-1'>
                 <p>New AI feature</p>
                 <img src={assets.star_icon} className='w-2.5' alt="" />
             </div>
+            <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700'>Your <span className='text-primary'>ideas</span> deserve <br /> <span className="block mt-3 sm:mt-4">a home</span></h1>
+            <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-700'>Create a personal blog that reflects your voice, not a platform’s agenda.</p>
+
+            <form className='flex justify-between max-w-lg max-sm:scale-75
+            mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+                <input className='w-full pl-4 outline-none' type="text" placeholder='Search for blogs' required />
+                <button type='submit' className='bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer'>Search</button>
+            </form>
         </div>
         <img src={assets.gradientBackground} className='absolute  -top-50 -z-1 opacity-50' alt="" />
     </div>
