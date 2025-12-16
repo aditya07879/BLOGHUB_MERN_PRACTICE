cddea6f5de18fd7da35dd55c1799fbb60889a883
aditya07879 <aditya07879@gmail.com>
2025-12-16 20:33:36 +0530

    Completed blogList

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
+import BlogList from '../Components/BlogList'
 
 function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <BlogList />
     </>
   )
 }
